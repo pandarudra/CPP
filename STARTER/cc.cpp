e710432c12336962f19c937b2fd70f511c100502
pandarudra <rudrapanda8206@gmail.com>
2024-12-19 10:37:46 +0530

    cp templete

---
+++ STARTER/cc.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define _inf INT_MIN
+#define  inf INT_MAX
+#define _linf LLONG_MIN
+#define  linf LLONG_MAX		
+
+typedef long long ll;
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+
+	return 0;
+}		
---
+++ STARTER/cf.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define _inf INT_MIN
+#define  inf INT_MAX
+#define _linf LLONG_MIN
+#define  linf LLONG_MAX		
+
+typedef long long ll;
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}		
